73ab77b5bb754e0c5fce3ab53a08a19d752b7467
mikhachev <36811961+mikhachev@users.noreply.github.com>
2020-10-04 09:54:30 +0300

    Add files via upload

---
+++ projects/recomendation_systems/src/metrics.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def precision(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    flags = np.isin(bought_list, recommended_list)
+
+    precision = flags.sum() / len(recommended_list)
+
+    return precision
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+
+    bought_list = bought_list  # Тут нет [:k] !!
+    
+    if k < len(recommended_list):
+        recommended_list = recommended_list[:k]
+
+    flags = np.isin(bought_list, recommended_list)
+
+    precision = flags.sum() / len(recommended_list)
+
+    return precision
+
+
+def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
+    # your_code
+    # Лучше считать через скалярное произведение, а не цикл
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    bought_list = bought_list  # Тут нет [:k] !!
+    recommended_list = recommended_list[:k]
+    
+    prices_recommended = np.array(prices_recommended)[:k]
+    
+    flags = np.isin(recommended_list, bought_list)
+    
+    precision = np.dot(flags,prices_recommended) / sum(prices_recommended)
+    
+    return precision
+
+
+def recall(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    flags = np.isin(bought_list, recommended_list)
+
+    recall = flags.sum() / len(bought_list)
+
+    return recall
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    if k < len(recommended_list):
+        recommended_list = recommended_list[:k]
+
+    flags = np.isin(bought_list, recommended_list)
+    recall = flags.sum() / len(bought_list)
+
+    return recall
+
+
+def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
+    # your_code
+
+    return recall
